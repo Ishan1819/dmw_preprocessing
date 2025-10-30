3ee827623c65b90c4d1038476c8130209650b34d
Ishan1819 <ishanp141@gmail.com>
2025-10-31 00:31:43 +0530

    commit

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from routers.preprocess_router import router as preprocess_router
 
+
 app = FastAPI()
 app.include_router(preprocess_router)
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
+# Add this middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 async def home():
